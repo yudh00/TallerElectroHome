d7b567b1d29ee327ae11920bc0d02a2935f5cfc8
Yudhansell Paniagua <yud.u.3011@gamil.com>
2025-07-06 20:55:18 -0600

    upd

--- datos/src/app/app.php
+++ datos/src/app/app.php
@@ -15,7 +15,7 @@ require 'config.php';
 $app->add(new Tuupola\Middleware\JwtAuthentication([
   "secure" => false,
   "path" => ["/api"],
-  "ignore" => ["/api/auth", "/api"],
+  "ignore" => ["/api/auth"],
   "secret" => ["acme" => $container->get('key')],
   "algorithm" => ["acme" => "HS256"],
 ]));
